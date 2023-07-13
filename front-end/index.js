4659f953083926debf0a89b875e57f8bc8f651c4
Ryan Earl <ryanleoearl@gmail.com>
2023-07-13 16:54:01 -0700

    Adding basics of site

---
+++ front-end/index.js
@@ -0,0 +1,44 @@
+directories = [
+  '',
+  'Contact'
+  
+]
+
+var getDirectories = directories;
+
+function createHead() {
+  const head = document.getElementById("head");
+  var title = document.createElement("Title");
+  title.innerHTML = "Ryan Earl Portfolio"
+  head.appendChild(title);
+}
+
+function createHeader() {
+  const header = document.getElementById("header");
+  var nav = document.createElement("nav");
+  var menu = document.createElement("div")
+  nav.setAttribute("id", "nav")
+  var name = document.createElement("a");
+  name.setAttribute("class", "companyName")
+  name.setAttribute("href", "/")
+  name.innerHTML = "Ryan Earl Portfolio"
+  nav.appendChild(name)
+  menu.setAttribute("class", "menu")
+  for (i = 0; i<= getDirectories.length-1; i++) {
+    var list = document.createElement("li");
+    var item = document.createElement("a");
+    item.setAttribute("class", "btn");
+    if (getDirectories[i] == "") {
+      getDirectories[i] = "Home";
+      item.setAttribute("href", "/");
+    }
+    else {
+      item.setAttribute("href", "/pages/"+getDirectories[i]+".html");
+    }
+    item.innerHTML = getDirectories[i];
+    list.appendChild(item)
+    menu.appendChild(list);
+    nav.appendChild(menu);
+  }
+  header.appendChild(nav);
+}
