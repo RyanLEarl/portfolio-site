eaab7a7164f29553ef39b8b3b91715a88fb82636
Ryan Earl <ryanleoearl@gmail.com>
2023-08-22 21:44:24 -0700

    processEmail properly parses incoming JSON and permissions have been updated to allow it to properly send emails

--- back-end/src/functions/processEmail.js
+++ back-end/src/functions/processEmail.js
@@ -2,7 +2,8 @@ const aws = require("aws-sdk");
 const ses = new aws.SES({ region: "us-west-2" });
 exports.handler = async function (event) {
   console.log('EVENT: ', event)
-  const { senderEmail, senderName, message } = JSON.parse(event.body)
+  const { senderEmail, senderName, message } = event.body
+  console.log(`Email: ${senderEmail} \n Name: ${senderName} \n Message: ${message}`)
   const params = {
     Destination: {
       ToAddresses: ["ryanleoearl@gmail.com"],
